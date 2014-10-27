0421dac0c1f131ce0241dc377973bc269e4a1340
John Otander <johnotander@gmail.com>
2014-10-26 21:06:10 -0600

    Add functionality.

---
+++ index.js
@@ -0,0 +1,20 @@
+var through     = require('through2');
+var remarkable  = require('remarkable ');
+
+module.exports = function(options) {
+  return through.obj(function(file, encoding, callback) {
+    if (!file.isBuffer()) {
+      callback();
+    }
+
+    var md = new remarkable('full');
+    var src = file.contents.toString();
+    var html = md.render(src);
+
+    console.log(html);
+
+    file.contents = new Buffer(html);
+    this.push(file);
+    callback();
+  });
+};
