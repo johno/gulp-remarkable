be99a2164304f25446ffe69ee91f388ffa680182
John Otander <johnotander@gmail.com>
2014-10-28 22:37:26 -0600

    Tweak the markdown string.

--- test.js
+++ test.js
@@ -10,14 +10,14 @@ it('should convert md to html', function(cb) {
 
   stream.once('data', function(file) {
     assert.equal(file.relative, 'test.html');
-    assert.equal(file.contents.toString(), '<h1>Hello, world</h1>\n');
+    assert.equal(file.contents.toString(), '<h1>Hello, world!</h1>\n');
   });
 
   stream.on('end', cb);
 
   stream.write(new gutil.File({
     path: 'test.md',
-    contents: new Buffer('# Hello, world')
+    contents: new Buffer('# Hello, world! #')
   }));
 
   stream.end();
