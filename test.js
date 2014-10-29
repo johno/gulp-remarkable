3763df7f7d61496137c512388e894997ad48dba7
John Otander <johnotander@gmail.com>
2014-10-28 22:26:20 -0600

    Add test.

---
+++ test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Adapted from https://github.com/sindresorhus/gulp-markdown/blob/master/test.js
+var assert = require('assert'),
+    gutil  = require('gulp-util'),
+    remark = require('./');
+
+it('should convert md to html', function(cb) {
+  var stream = remark();
+
+  stream.once('data', function(file) {
+    assert.equal(file.contents.toString(), '<h1>Hello, world!</h1>\n');
+  });
+
+  stream.on('end', cb);
+
+  stream.write(new gutil.File({
+    path: 'text.md',
+    contents: new Buffer('# Hello, world!')
+  }));
+
+  stream.end();
+});
