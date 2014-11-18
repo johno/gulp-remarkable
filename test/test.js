8c9990caa3d85250565aa4df014b8e15ca957f54
John Otander <johnotander@gmail.com>
2014-11-17 23:59:38 -0700

    Default to full, update tests for latest version

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 /**
- * gulp-remarkable <https://github.com/tunnckoCore/gulp-remarkable>
+ * gulp-remarkable <https://github.com/johnotander/gulp-remarkable>
  *
  * Copyright (c) 2014 John Otander, contributors.
  * Released under the MIT license.
@@ -34,13 +34,35 @@ describe('gulp-remarkable should convert md to html', function() {
     mdStream.end();
   })
 
-  it('with `preset:full`, `typographer:true` (copyright, plusminus, ellipsis)', function(done) {
+  it('with `preset:full`, `typographer:true` disable:["replacements"]', function(done) {
     var mdStream = remarkableStream({
-      typographer: {
-        copyright:    true, // (c) (C) → ©
-        plusminus:    true, // +- → ±
-        ellipsis:     true, // ... → … (also ?.... → ?.., !.... → !..)
-      },
+      disable: [
+        'replacements'
+      ],
+      remarkableOptions: {
+        typographer: true
+      }
+    });
+    var actual = '# Hello, world!\nCopyright +- (c) year? ...';
+    var expect = '<h1>Hello, world!</h1>\n<p>Copyright +- (c) year? ...</p>\n'
+
+    mdStream.once('data', function(file) {
+      assert.equal(file.relative, 'typographer.html');
+      assert.equal(file.contents.toString(), expect);
+    });
+    mdStream.on('end', done);
+
+    mdStream.write(new gutil.File({
+      path: 'typographer.md',
+      contents: new Buffer(actual)
+    }));
+
+    mdStream.end();
+  })
+
+  it('with `preset:full`, `typographer:true` disable:[]', function(done) {
+    var mdStream = remarkableStream({
+      disable: [],
       remarkableOptions: {
         typographer: true
       }
